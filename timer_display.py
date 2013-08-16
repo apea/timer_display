bc00b3aa91255a7d10fc7dbd9a4789b74bda610d
apea <Andrei.Apetrei.A@gmail.com>
2013-08-16 17:01:54 -0400

    Fonctionnal timer
    A version of a basic timer that works!

--- timer_display.py
+++ timer_display.py
@@ -1,39 +1,32 @@
 import sys
-import time
-import os
 from PySide import QtGui, QtCore
-
-class Timer:
-
- def __init__(self):
- 
-  self._timeOrigin = time.time()
-  self.comppt = int(self._timeOrigin-time.time())
 
 class Display(QtGui.QWidget):
 
  def __init__(self):
  
   super(Display, self).__init__()
-
   self.initUI()
 
  def initUI(self):
- 
-  lcd = QtGui.QLCDNumber(self)
+  self.lcd = QtGui.QLCDNumber(self)
   
   box = QtGui.QVBoxLayout()
-  box.addWidget(lcd)
+  box.addWidget(self.lcd)
   
   self.setLayout(box)
-
-  timer_display=Timer()
+  self.time=0
+  self.timer = QtCore.QBasicTimer()
+  self.timer.start(1000, self)
   self.show()
   
+ def timerEvent(self, e):
+  self.time = self.time + 1
+  self.lcd.display(self.time)
+  
 def main():
-
- app=QtGui.QApplication(sys.argv)
- ex=Display()
+ app = QtGui.QApplication(sys.argv)
+ ex = Display()
  sys.exit(app.exec_())
  
 if __name__=='__main__':
