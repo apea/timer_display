db6b203f5318018a62eea6df9fa55d79a6886a44
apea <Andrei.Apetrei.A@gmail.com>
2013-08-19 11:01:47 -0400

    Realistic Timer

--- timer_display.py
+++ timer_display.py
@@ -15,6 +15,7 @@ class Display(QtGui.QWidget):
   box.addWidget(self.lcd)
   
   self.setLayout(box)
+  self.setGeometry(300, 300, 290, 150)
   self.time=0
   self.timer = QtCore.QBasicTimer()
   self.timer.start(1000, self)
@@ -22,7 +23,29 @@ class Display(QtGui.QWidget):
   
  def timerEvent(self, e):
   self.time = self.time + 1
-  self.lcd.display(self.time)
+  self.lcd.display(self.showTime())
+  
+ def showTime(self):
+  self.sec = self.time%60
+  self.min = self.time//60
+  
+  if self.min < 10 and self.min > 0:
+   self.showmin = '0' + str(self.min)
+  elif self.min == 0:
+   self.showmin = "00"
+  else:  
+   self.showmin = str(self.min)
+  
+  if self.sec < 10 and self.sec > 0:
+   self.showsec = '0' + str(self.sec)
+  elif self.sec == 0:
+   self.showsec = "00" 
+  else:  
+   self.showsec = str(self.sec)
+   
+  self.show = self.showmin + ':' + self.showsec
+  
+  return self.show
   
 def main():
  app = QtGui.QApplication(sys.argv)
